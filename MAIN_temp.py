273a1739c3921b66a38b2f7990a26ac97f115eb6
kano MAHAMADOU <kanoibe2004@gmail.com>
2025-02-12 19:03:52 +0000

    Test avec MAIN_temp.py

---
+++ MAIN_temp.py
@@ -0,0 +1,26 @@
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+number = "+221778391417"
+
+# Parser le numéro
+numero = phonenumbers.parse(number)
+
+# Afficher l'objet PhoneNumber
+print("Numéro parsé:", numero)
+
+# Vérifier si le numéro est valide
+is_valid = phonenumbers.is_valid_number(numero)
+print("Le numéro est-il valide ?", is_valid)
+
+# Récupérer la localisation
+location = geocoder.description_for_number(numero, "fr")
+print("Location:", location)
+
+#recuperer le nom de l'operateur 
+
+operateur = phonenumbers.parse(number)
+print(carrier.name_for_number(operateur, "fr"))
+
+
